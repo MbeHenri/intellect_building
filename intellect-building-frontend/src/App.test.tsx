b6a105a128a1bf22783457d71d4d7d3836f1769e
MbeHenri <henri.mbe@facsciences-uy1.cm>
2024-08-09 09:36:16 +0100

    handle pnpm packager

--- intellect-building-frontend/src/App.test.tsx
+++ intellect-building-frontend/src/App.test.tsx
@@ -4,5 +4,5 @@ import App from "./App";
 test("renders header of first section", () => {
   render(<App />);
   const header = screen.getByText(/What We Do/i);
-  expect(header).toBeInTheDocument();
+  expect(header).toBeDefined();
 });
